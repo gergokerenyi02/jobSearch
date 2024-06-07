0a3e1fbd8fb79da707e536d6c92c636ed92d9ed5
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-07 17:52:52 +0200

    Added imports

--- src/main/java/com/example/jobsearch/controller/ClientController.java
+++ src/main/java/com/example/jobsearch/controller/ClientController.java
@@ -1,5 +1,11 @@
 package com.example.jobsearch.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
 
 public class ClientController {
 
