0ffd672c93124d0b9a094bc88a9c87ab23468408
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-08 19:00:46 +0200

    Extended the ViewController to display status message when adding new client

--- src/main/java/com/example/jobsearch/controller/ViewController.java
+++ src/main/java/com/example/jobsearch/controller/ViewController.java
@@ -1,5 +1,6 @@
 package com.example.jobsearch.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ViewController {
 
     @GetMapping("/")
+    public String index(HttpSession session, Model model) {
+        String message = (String) session.getAttribute("message");
+        model.addAttribute("welcome_message", "Welcome to Job Search Application!");
+        if (message != null) {
 
-    public String index(Model model) {
-        model.addAttribute("message", "Welcome to Job Search Application!");
+            model.addAttribute("message", message);
+            session.removeAttribute("message");
+        }
+
         return "index";
     }
 }
