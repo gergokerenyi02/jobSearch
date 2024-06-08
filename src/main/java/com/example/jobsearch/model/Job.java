85543df509e7c27f0270f0efac15dffe4392414d
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-08 15:37:07 +0200

    Getter / Setter fixed

--- src/main/java/com/example/jobsearch/model/Job.java
+++ src/main/java/com/example/jobsearch/model/Job.java
@@ -1,43 +1,15 @@
 package com.example.jobsearch.model;
 
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter @Setter
 public class Job {
     private Long id;
     private String title;
     private String location;
     private String description;
 
-    // Getters and setters
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
