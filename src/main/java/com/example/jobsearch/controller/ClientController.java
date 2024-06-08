f10433ce7c3b60a7b3f7688950663929f1510d9f
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-08 12:43:06 +0200

    ClientController Class created and comment added

--- src/main/java/com/example/jobsearch/controller/ClientController.java
+++ src/main/java/com/example/jobsearch/controller/ClientController.java
@@ -1,12 +1,26 @@
 package com.example.jobsearch.controller;
 
+import com.example.jobsearch.model.Client;
+import com.example.jobsearch.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// Controller réteg
+// Megkapja a bejövő HTTP kéréseket és meghatározza milyen műveletet hajtson végre
 
-@RestController
-
+@RestController // Returns JSON
+@RequestMapping("/client")
 public class ClientController {
+
+    // ClientService Bean-t megkeresi a Spring és injektálja
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<String> registerClient(@RequestBody Client client) {
+        String apiKey = clientService.registerClient(client);
+        return ResponseEntity.ok(apiKey);
+    }
 
 }
