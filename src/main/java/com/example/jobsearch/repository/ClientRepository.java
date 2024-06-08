697399efa8980628d5d121d333d57ae03d3f5630
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-08 16:36:13 +0200

    Marked class as a Repository and added comment

--- src/main/java/com/example/jobsearch/repository/ClientRepository.java
+++ src/main/java/com/example/jobsearch/repository/ClientRepository.java
@@ -1,7 +1,14 @@
 package com.example.jobsearch.repository;
 
+
 import com.example.jobsearch.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+// Adat-hozzáférési komponens (DAO)
+// JpaRepository<Client, Long> -> INTERFACE (Spring Data JPA)
+// biztosítja az alapvető adat-hozzáférési műveletekhez szükséges metódusokat (save, findAll, findById, deleteById)
+
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
