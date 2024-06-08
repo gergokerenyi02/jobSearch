842f327c659b8b518488d149f38e445e1c5ba221
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-08 18:57:09 +0200

    Changed from RestController to Controller to return HTML

--- src/main/java/com/example/jobsearch/controller/ClientController.java
+++ src/main/java/com/example/jobsearch/controller/ClientController.java
@@ -2,20 +2,28 @@ package com.example.jobsearch.controller;
 
 import com.example.jobsearch.model.Client;
 import com.example.jobsearch.service.ClientService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
 
 // Controller réteg
 // Megkapja a bejövő HTTP kéréseket és meghatározza milyen műveletet hajtson végre
 
-@RestController // Returns JSON
+//@RestController // Returns JSON
+@Controller
 @RequestMapping("/client")
 public class ClientController {
 
     // ClientService Bean-t megkeresi a Spring és injektálja
     @Autowired
     private ClientService clientService;
+    @Autowired
+    private HttpSession httpSession;
 
 
     // @RequestBody a kapott JSON-t a index.html form post kérelemtől automatikusan deszerializálja és feltölti a Client object-tet a megfelelő mezőkkel
@@ -30,9 +38,14 @@ public class ClientController {
     // Használjunk @ModelAttribute annotációt
 
     @PostMapping
-    public ResponseEntity<String> registerClient(@ModelAttribute Client client) {
+    public ModelAndView registerClient(@ModelAttribute Client client) {
         String apiKey = clientService.registerClient(client);
-        return ResponseEntity.ok(apiKey);
+
+        httpSession.setAttribute("apiKey", apiKey);
+        httpSession.setAttribute("message", "User created successfully!");
+
+        RedirectView redirectView = new RedirectView("/");
+        return new ModelAndView(redirectView);
     }
 
 }
