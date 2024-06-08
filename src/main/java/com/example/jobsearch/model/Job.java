b8221de4750f22081e6cac4cd6f2362c0480531f
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-08 15:35:32 +0200

    Job class implemented

---
+++ src/main/java/com/example/jobsearch/model/Job.java
@@ -0,0 +1,43 @@
+package com.example.jobsearch.model;
+
+
+public class Job {
+    private Long id;
+    private String title;
+    private String location;
+    private String description;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
