d96a1bb6e23c0e4c5bd122b19bef0d45c576b458
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-09 13:47:44 +0200

    Rewrote to RestAPI

--- src/main/java/com/example/jobsearch/controller/ClientController.java
+++ src/main/java/com/example/jobsearch/controller/ClientController.java
@@ -5,8 +5,11 @@ import com.example.jobsearch.service.ClientService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -14,8 +17,8 @@ import org.springframework.web.servlet.view.RedirectView;
 // Controller réteg
 // Megkapja a bejövő HTTP kéréseket és meghatározza milyen műveletet hajtson végre
 
-//@RestController // Returns JSON
-@Controller
+@RestController // Returns JSON
+//@Controller // Returns HTML
 @RequestMapping("/client")
 public class ClientController {
 
@@ -38,14 +41,15 @@ public class ClientController {
     // Használjunk @ModelAttribute annotációt
 
     @PostMapping
-    public ModelAndView registerClient(@ModelAttribute Client client) {
+    public ResponseEntity<?> registerClient(@Validated @ModelAttribute Client client) {
         String apiKey = clientService.registerClient(client);
 
         httpSession.setAttribute("apiKey", apiKey);
         httpSession.setAttribute("message", "User created successfully!");
 
-        RedirectView redirectView = new RedirectView("/");
-        return new ModelAndView(redirectView);
+        //RedirectView redirectView = new RedirectView("/");
+        //return new ModelAndView(redirectView);
+        return ResponseEntity.ok(apiKey);
     }
 
 }
