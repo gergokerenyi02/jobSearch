a41ffac61b0fd1a5b7ce54334fd083b879a8031b
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-09 13:51:50 +0200

    Added swagger-ui config

---
+++ src/main/java/com/example/jobsearch/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.jobsearch.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Job Search Application API")
+                        .version("1.0")
+                        .description("API documentation for Job Search Application"));
+    }
+}
