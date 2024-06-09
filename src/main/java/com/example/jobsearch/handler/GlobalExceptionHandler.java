f6f1533f4e74081b36ffc11aab80a0b47c3a9eb9
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-09 18:19:57 +0200

    Implemented global exception class

---
+++ src/main/java/com/example/jobsearch/handler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.jobsearch.handler;
+
+import com.example.jobsearch.exception.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+// Kivétel kezelő osztály
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
