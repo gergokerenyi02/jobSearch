d3680a527ff26a211b6cb3a714e186d4d39163d8
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-09 19:20:50 +0200

    Added custom MethodArgumentNotValidException

--- src/main/java/com/example/jobsearch/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/jobsearch/handler/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.example.jobsearch.handler;
 import com.example.jobsearch.exception.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 // Kivétel kezelő osztály
 
@@ -14,5 +19,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<String> handleValidationException(ValidationException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        StringBuilder errorMessage = new StringBuilder();
+
+        for (FieldError fieldError : fieldErrors) {
+            errorMessage.append(fieldError.getField()).append(": ").append(fieldError.getDefaultMessage()).append(", ");
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage.toString());
     }
 }
