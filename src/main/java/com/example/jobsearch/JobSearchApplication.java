d3d2c87243eeb64a43e7dabdeed0297db66e5b7a
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-10 18:16:51 +0200

    Added EnableSwagger

--- src/main/java/com/example/jobsearch/JobSearchApplication.java
+++ src/main/java/com/example/jobsearch/JobSearchApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableConfigurationProperties
+@EnableSwagger2
 @EntityScan("com.example.jobsearch.model")
 @EnableJpaRepositories(basePackages = {"com.example.jobsearch"})
 public class JobSearchApplication {
