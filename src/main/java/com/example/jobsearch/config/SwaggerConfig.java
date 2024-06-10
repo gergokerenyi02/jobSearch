d8a5ac870f967ccb0e7b6521ef89005a204e3099
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-10 18:17:24 +0200

    changed swaggerconfig

--- src/main/java/com/example/jobsearch/config/SwaggerConfig.java
+++ src/main/java/com/example/jobsearch/config/SwaggerConfig.java
@@ -13,10 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.jobsearch.controller"))
+                .paths(PathSelectors.any()).build();
     }
 }
