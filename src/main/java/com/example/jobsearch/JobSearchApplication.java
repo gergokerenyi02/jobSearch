7db74d55eb5f8c41c91daedc1f45b09e2ac9f413
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-06-10 17:23:30 +0200

    Fixed database not created error

--- src/main/java/com/example/jobsearch/JobSearchApplication.java
+++ src/main/java/com/example/jobsearch/JobSearchApplication.java
@@ -3,9 +3,15 @@ package com.example.jobsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableConfigurationProperties
 @EntityScan("com.example.jobsearch.model")
+@EnableJpaRepositories(basePackages = {"com.example.jobsearch"})
 public class JobSearchApplication {
 
     public static void main(String[] args) {
