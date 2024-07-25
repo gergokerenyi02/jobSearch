eb18b9ff534492da7274123e2ea1af53accc67c2
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-07-25 20:47:10 +0200

    Removed comments

--- src/main/java/com/example/jobsearch/controller/ClientController.java
+++ src/main/java/com/example/jobsearch/controller/ClientController.java
@@ -11,42 +11,23 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
-
-
-// Controller réteg
-// Megkapja a bejövő HTTP kéréseket és meghatározza milyen műveletet hajtson végre
-
-@RestController // Returns JSON
-//@Controller // Returns HTML
+@RestController
 @RequestMapping("/client")
 public class ClientController {
 
-    // ClientService Bean-t megkeresi a Spring és injektálja
     @Autowired
     private ClientService clientService;
     @Autowired
     private HttpSession httpSession;
 
 
-    // @RequestBody a kapott JSON-t a index.html form post kérelemtől automatikusan deszerializálja és feltölti a Client object-tet a megfelelő mezőkkel
-    // A ClientService registerClient metódusa meghívásra kerül ezzel a Client-tel.
-    // Return: egy api kulcs
-
-    // Error: @RequestBody miatt
-    // Form küldési típusa: (Content-Type) application/x-www-form-urlencoded
-    // A @RequestBody JSON-t vár
-
-    // Megoldás
-    // Használjunk @ModelAttribute annotációt
-
     @PostMapping
     public ResponseEntity<?> registerClient(@Validated @RequestBody Client client) {
-        String apiKey = clientService.registerClient(client);
 
+        String apiKey = clientService.registerClient(client);
         httpSession.setAttribute("apiKey", apiKey);
 
         return new ResponseEntity<>("Client registered successfully." , HttpStatus.OK);
-
     }
 
 }
