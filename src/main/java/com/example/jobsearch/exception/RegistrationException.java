566df70f87524288bc138183701a011fe9d3340b
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-07-25 21:49:25 +0200

    Created RegistrationException class

---
+++ src/main/java/com/example/jobsearch/exception/RegistrationException.java
@@ -0,0 +1,18 @@
+package com.example.jobsearch.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class RegistrationException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public RegistrationException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+
+
+}
