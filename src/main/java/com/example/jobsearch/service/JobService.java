a471928b6eac28a0c9b81356e2b780b8d757652b
Gergo Kerenyi <gergokerenyi02@gmail.com>
2024-07-28 19:53:11 +0200

    Added status to exception message.

--- src/main/java/com/example/jobsearch/service/JobService.java
+++ src/main/java/com/example/jobsearch/service/JobService.java
@@ -5,6 +5,7 @@ import com.example.jobsearch.model.Job;
 import java.util.List;
 import com.example.jobsearch.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 // Állás létrehozása: VALIDÁCIÓ, API kulcs ellenőrzése, Adatbázisba mentés
@@ -20,10 +21,10 @@ public class JobService {
     public Job createJob(Job job) {
 
         if(job.getTitle() == null || job.getTitle().isEmpty()) {
-            throw new ValidationException("Title cannot be empty. Enter a valid title to create a new job record.");
+            throw new ValidationException(HttpStatus.BAD_REQUEST, "Title cannot be empty. Enter a valid title to create a new job record.");
         }
         if(job.getLocation() == null || job.getLocation().isEmpty()) {
-            throw new ValidationException("Location cannot be empty. Enter a valid location to create a new job record.");
+            throw new ValidationException(HttpStatus.BAD_REQUEST, "Location cannot be empty. Enter a valid location to create a new job record.");
         }
 
         return jobRepository.save(job);
